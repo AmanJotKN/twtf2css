27018fa90d5e084509b930f505ceb8ab3c233063
amanjotKN <amanjot@dice-academy.com>
2024-11-27 15:42:40 +0530

    vss day2

---
+++ js/day1.js
@@ -0,0 +1,8 @@
+// console.log("tinku")
+// let monitor="tinku"
+// console.log(monitor)
+let age=22-2
+let student1="ayush"
+console.log(age-1,student1, "jsjsdjd")
+console.log(22/2)
+console.log(23%2);
